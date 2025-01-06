35826c5ce8834aaeeb5b5dc49e0d13a483b5b356
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2025-01-07 02:00:16 +0900

    Time: 147 ms (6.25%), Space: 39.6 MB (6.25%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun minOperations(boxes: String): IntArray {
+        var left = 0
+        var right = 0
+        var sum = 0;
+        for (i in 1..<boxes.length) {
+            if (boxes[i] == '1') {
+                right++
+                sum += i
+            }
+        }
+
+        val ret = IntArray(boxes.length)
+        ret[0] = sum
+        if (boxes[0] == '1') {
+            left++
+        }
+        println("$sum, $left, $right")
+        for (i in 1..<boxes.length) {
+
+            sum = sum + left - right
+            println("$sum, $left, $right")
+            ret[i] = sum
+
+            if (boxes[i] == '1') {
+                left++
+                right--
+            }
+        }
+        return ret
+    }
+}
