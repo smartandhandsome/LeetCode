e68b417691d5453f016ccc70a70d56c94c1b3d53
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2025-03-19 00:18:41 +0900

    Time: 3 ms (98.64%), Space: 55.4 MB (86.38%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,36 +1,20 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
         int left = 0;
-        int right = nums.length;
+        int right = 1;
 
         int answer = 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (check(nums, mid)) {
-                answer = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int bitMask = nums[left];
+        while (right < nums.length && left <= right) {
+            while ((bitMask & nums[right]) != 0) {
+                bitMask ^= nums[left];
+                left++;
             }
+
+            bitMask |= nums[right];
+            answer = Math.max(answer, right - left + 1);
+            right++;
         }
         return answer;
-    }
-
-    private boolean check(int[] nums, int mid) {
-        for (int start = 0; start + mid <= nums.length; start++) {
-            int my = 0;
-            boolean isNice = true;
-            for (int i = start; i - start < mid; i++) {
-                if ((my & nums[i]) != 0) {
-                    isNice = false;
-                    break;
-                }
-                my |= nums[i];
-            }
-            if (isNice) {
-                return true;
-            }
-        }
-        return false;
     }
 }
