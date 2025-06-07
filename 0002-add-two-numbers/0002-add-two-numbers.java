745598e583316fb18a31d9cb61e976cbefefdbe9
Sangmin Park <70051888+smartandhandsome@users.noreply.github.com>
2025-06-07 17:11:34 +0900

    Time: 1 ms (100%), Space: 44.8 MB (20.91%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,28 +8,68 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
+class Solution {
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        int carry = 0;
-        ListNode node = head;
-        while (l1 != null || l2 != null) {
-            int sum = carry;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            node.next = new ListNode(sum % 10);
-            node = node.next;
-            carry = sum / 10;
+        ListNode first = new ListNode();
+        
+        ListNode prev = null;
+        ListNode node = first;
+        boolean up = false;
+        while(l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + (up ? 1 : 0);
+            up = (sum / 10 == 1);
+            
+            int val = sum % 10;
+            node.val = val;
+
+            prev = node;
+            ListNode nextNode = new ListNode();
+            node.next = nextNode;
+            node = nextNode;
+
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        if (carry > 0){
-            node.next = new ListNode(carry);
+        if (l1 == null && l2 == null) {
+            if (up) {
+                node.val = 1;
+            } else {
+                prev.next = null;
+            }
+            return first;
         }
-        return head.next;
+
+        calc(l1, node, up);
+        calc(l2, node, up);
+        return first;
     }
+
+    public void calc(ListNode list, ListNode node, boolean up) {
+        if (node == list) {
+            return;
+        }
+
+        while(list != null) {
+            int sum = list.val + (up ? 1 : 0);
+            up = sum == 10;
+            node.val = sum % 10;
+            
+            ListNode nextNode = new ListNode();
+            
+            if(list.next == null) {
+                if (up) {
+                    node.next = nextNode;
+                    nextNode.val = 1;
+                }
+                return;
+            }
+
+            node.next = nextNode;
+            node = nextNode;
+            
+            list = list.next;
+        }
+    }
+        
 }
